f6f66d98d27f08232f5e207cb35975ce385613bb
Inna Khvan <in.khvan@bk.ru>
2023-03-14 08:48:47 +0900

    add Header.Module.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,7 @@ module.exports = {
       secondary3: "#FFC34F",
       secondary4: "#EF8F00",
     },
-    container: { 
+    container: {
       center: true,
 
       // padding: '1rem',
@@ -29,7 +29,5 @@ module.exports = {
       },
     },
   },
-
-    plugins: [],
-  },
+  plugins: [],
 };
