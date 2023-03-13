51a7f932bd4b0b4c728ba7aff0d2e48db303a032
Inna Khvan <in.khvan@bk.ru>
2023-03-13 13:53:12 +0900

    add main file and folder

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { store } from "./store";
+import { Provider as StoreProvider } from "react-redux";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+      <StoreProvider store={store}>
+    <RouterProvider router={router} />
+    </StoreProvider>
+  </React.StrictMode>
+);
