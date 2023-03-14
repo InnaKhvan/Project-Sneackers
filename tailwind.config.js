becca7641ed41161673f98ed03be5dab0a8809b0
Inna Khvan <in.khvan@bk.ru>
2023-03-14 19:25:10 +0900

    change tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,48 +4,20 @@ module.exports = {
   theme: {
     extend: {},
     screens: {
-      sm: '375px',
-      md: '768px',
-      xl: '1440px',
+      sm: "375px",
+      md: "768px",
+      xl: "1440px",
     },
     colors: {
-      transparent: 'transparent',
-      black: '#000',
-      white: '#fff',
-      gray: {
-        110:  '#F1F1F1',
-        910: '#444444',
-      },
-      red: '#FF0000',
-  },
-  fontSize: {
-    xs: {
-      size: '12px',
-      height: '14.63px',
-  },
+      primary1: "#0A0A0A",
+      primary2: "#FFA800",
+      secondary1: "#F1F1F1",
+      secondary2: "#F1F1F1",
+      sale: "#FF0000",
+      secondary3: "#FFC34F",
+      secondary4: "#EF8F00",
+    },
 
+    plugins: [],
   },
-  height: {
-    13: '55px',
-    37: '156px',
-    53: '211px',
-  },
-  width: {
-84: '346px',
-  },
-  padding: {
-    pl: {
-      calc1: 'calc(50% - 1440px/2)',
-    },
-  },
-  margin: {
-    ml: {
-      calc1: 'calc(50% - 1440px/2)',
-      1290:'1290px'
-    },
-  },
-  borderRadius:{
-    rounded10: '10px', 
-  },
-  plugins: [],
-}}
+};
