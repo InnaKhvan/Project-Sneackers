1c6b44582f1703c7810142f2450dce81e22c46c0
Inna Khvan <in.khvan@bk.ru>
2023-03-14 19:25:10 +0900

    change tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,9 @@ module.exports = {
   theme: {
     extend: {},
     screens: {
-      mobile: "375px",
-      tablet: "768px",
-      desktop: "1440px",
+      sm: "375px",
+      md: "768px",
+      xl: "1440px",
     },
     colors: {
       primary1: "#0A0A0A",
@@ -17,19 +17,29 @@ module.exports = {
       secondary3: "#FFC34F",
       secondary4: "#EF8F00",
     },
-    container: { 
-      center: true,
 
-      // padding: '1rem',
-
-      screens: {
-        mobile: "100%",
-        tablet: "704px",
-        desktop: "1200px",
-      },
+  },
+  height: {
+    13: '55px',
+    37: '156px',
+    53: '211px',
+  },
+  width: {
+84: '346px',
+  },
+  padding: {
+    pl: {
+      calc1: 'calc(50% - 1440px/2)',
     },
   },
-
-    plugins: [],
+  margin: {
+    ml: {
+      calc1: 'calc(50% - 1440px/2)',
+      1290:'1290px'
+    },
   },
-};
+  borderRadius:{
+    rounded10: '10px', 
+  },
+  plugins: [],
+}}
