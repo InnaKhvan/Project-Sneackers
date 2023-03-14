25ee5d44cf8ee1b24300010dbe68f86fe8821984
Inna Khvan <in.khvan@bk.ru>
2023-03-14 19:50:08 +0900

    change tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,21 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    screens: {
+      sm: "375px",
+      md: "768px",
+      xl: "1440px",
+    },
+    colors: {
+      primary1: "#0A0A0A",
+      primary2: "#FFA800",
+      secondary1: "#F1F1F1",
+      secondary2: "#F1F1F1",
+      sale: "#FF0000",
+      secondary3: "#FFC34F",
+      secondary4: "#EF8F00",
+    },
+
+    plugins: [],
   },
-  plugins: [],
-}
+};
