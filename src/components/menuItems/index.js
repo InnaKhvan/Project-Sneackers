60ff00adb51270174b2ecf183ce1b67bf87aab91
Inna Khvan <in.khvan@bk.ru>
2023-03-15 23:06:39 +0900

    add MainMenuItem

--- src/components/menuItems/index.js
+++ src/components/menuItems/index.js
@@ -1,21 +1,17 @@
 import { Link } from "react-router-dom";
+import MainMenuItem from "../MainMenuItem";
 import styles from "./menu.module.css";
 const MenuItems = () => {
   return (
-    <div className="flex font-semibold h-14 space-x-9 text-xs bg-secondary1 ">
+    <div className="flex h-14 text-xs bg-secondary1 ">
       <div
-        className={"flex justify-between flex-row items-center " + styles.menu}
+        className={"flex items-center " + styles.menu}
       >
-        <div className="bg-current my-5">
-          <Link>МУЖСКИЕ</Link>
+          <Link><MainMenuItem /></Link>
         </div>
-        <div className="my-5">
-          <Link>ЖЕНСКИЕ</Link>
-        </div>
-        <Link className="flex my-5">БРЕНДЫ</Link>
-        <Link className="flex my-5 text-sale">HOT SALE</Link>
+        
       </div>
-    </div>
+  
   );
 };
 export default MenuItems;
