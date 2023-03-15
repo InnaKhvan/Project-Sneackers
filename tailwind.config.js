59ddddf34bcd12017bd66eb6e09049bc04966615
Inna Khvan <in.khvan@bk.ru>
2023-03-15 21:43:33 +0900

    add screens in taiwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,14 @@ module.exports = {
   theme: {
     extend: {},
     screens: {
-      sm: "375px",
-      md: "768px",
-      xl: "1440px",
+      tablet: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      laptop: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      desktop: "1280px",
+      // => @media (min-width: 1280px) { ... }
     },
     colors: {
       primary1: "#0A0A0A",
