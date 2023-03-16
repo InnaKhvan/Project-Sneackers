ed1a5810ee50baf482f42ecd588a8e6b3bf077ab
Inna Khvan <in.khvan@bk.ru>
2023-03-16 23:40:10 +0900

    add HeartIcon, ShopCartIcon, UserIcon change SearchIcon

--- src/components/SearchIcon/index.js
+++ src/components/SearchIcon/index.js
@@ -1,15 +1,12 @@
 const SearchIcon = () => {
   return (<>
   <div>
-    <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M9 17C13.4183 17 17 13.4183 17 9C17 4.58172 13.4183 1 9 1C4.58172 1 1 4.58172 1 9C1 13.4183 4.58172 17 9 17Z" stroke="#7A7A7A" stroke-linecap="round" stroke-linejoin="round"/>
-    </svg>
-    <svg width="6" height="6" viewBox="0 0 6 6" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M4.99999 4.9999L0.649994 0.649902" stroke="#7A7A7A" stroke-linecap="round" stroke-linejoin="round"/>
+  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M9 17C13.4183 17 17 13.4183 17 9C17 4.58172 13.4183 1 9 1C4.58172 1 1 4.58172 1 9C1 13.4183 4.58172 17 9 17Z" stroke="#7A7A7A" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M19 18.9999L14.65 14.6499" stroke="#7A7A7A" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>
+
 </div>
-
-
 
 </>
   );
