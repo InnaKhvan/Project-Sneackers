fb1f22a1df317e6fde6149e4b0be20e5c61844fa
Inna Khvan <in.khvan@bk.ru>
2023-03-17 17:40:05 +0900

    changes MainMenuItem, Header

--- src/components/MainMenuItem/index.js
+++ src/components/MainMenuItem/index.js
@@ -1,16 +1,15 @@
 import { Link } from "react-router-dom";
 import items from "../../mockData/menuItem.json";
-import styles from "./MainMenuItem.module.css";
 const MainMenuItem = () => {
-  return items.map((item) => (
-    <div className="flex h-14 text-xs bg-secondary1 ">
-      <div className={"flex items-center " + styles.menu}>
+  return (
+    <div className="container flex items-center h-14 text-xs bg-secondary1 ">
+      {items.map((item) => (
         <Link>
           <button className="p-9">{item.name}</button>
         </Link>
-      </div>
+      ))}
     </div>
-  ));
+  );
 };
 
 export default MainMenuItem;
