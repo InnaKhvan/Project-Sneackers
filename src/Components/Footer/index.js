b2e169ec021718d89be5a4961ac5c31595424e84
Dean1501 <damdus@bk.ru>
2023-03-18 22:34:50 +0300

    create Footer

---
+++ src/Components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Container } from "react-dom";
+import styles from "./Footer.css";
+
+function Footer() {
+    return (
+        <Container 
+    );
+}
+
+export default Footer;
