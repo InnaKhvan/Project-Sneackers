e90ce641d9f55b1477a64c8dd07959c12230ed16
Dean1501 <damdinbatorov1992@gmail.com>
2023-03-21 11:58:18 +0300

    add columns-3

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -7,36 +7,32 @@ import InstagramIcon from "../svg/InstagramIcon";
 
 function Footer() {
     return (
-        <header className="flex flex-col h-53">
-            <div className="container flex h-37 flex-row items-center justify-around">
-                <div className={"flex justify-between items-center " + styles.header}>
-                    <LogoIcon />
-                    <h4>Контакты</h4>
-                    <u1 className="listFooter">
-                        <li><a href="tel:375337777171">+375 (33) 777  71 71</a></li>
-                        <li><a href="a">sneackers.by@gmail.com</a></li>
-                        <li><a href="a">Беларусь, г. Минск, ул. Западная 34/1</a></li>
-                        <li><a href="a">пн-пт 10:00-20:00</a></li>
-                    </u1>
-                    <h4>Информация</h4>
-                    <u1 className="listFooter">
-                        <li><a href="a">О компании</a></li>
-                        <li><a href="a">Таблица размеров</a></li>
-                        <li><a href="a">Уход за товаром</a></li>
-                        <li><a href="a">Политика конфиденциальности</a></li>
-                    </u1>
-                    <h4>Помощь</h4>
-                    <u1 className="listFooter">
-                        <li><a href="a">Оформление заказа</a></li>
-                        <li><a href="a">Доставка и оплата</a></li>
-                        <li><a href="a">Возврат и обмен</a></li>
-                    </u1>
-                    <TwitterIcon />;
-                    <FacebookIcon />;
-                    <InstagramIcon />;
-                </div>
-            </div>
-        </header>
+        <div class="columns-3 ...">
+            <LogoIcon />
+            <h4>Контакты</h4>
+            <u1 className="listFooter">
+                <li><a href="tel:375337777171">+375 (33) 777  71 71</a></li>
+                <li><a href="a">sneackers.by@gmail.com</a></li>
+                <li><a href="a">Беларусь, г. Минск, ул. Западная 34/1</a></li>
+                <li><a href="a">пн-пт 10:00-20:00</a></li>
+            </u1>
+            <h4>Информация</h4>
+            <u1 className="listFooter">
+                <li><a href="a">О компании</a></li>
+                <li><a href="a">Таблица размеров</a></li>
+                <li><a href="a">Уход за товаром</a></li>
+                <li><a href="a">Политика конфиденциальности</a></li>
+            </u1>
+            <h4>Помощь</h4>
+            <u1 className="listFooter">
+                <li><a href="a">Оформление заказа</a></li>
+                <li><a href="a">Доставка и оплата</a></li>
+                <li><a href="a">Возврат и обмен</a></li>
+            </u1>
+            <TwitterIcon />;
+            <FacebookIcon />;
+            <InstagramIcon />;
+        </div>
     );
 };
 
