acb66025e3ceb51045a49b508ef3c5b07014fa16
Dean1501 <damdus@bk.ru>
2023-03-26 20:44:49 +0300

    rename components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,6 @@
 import "./index.css";
 import Footer from "./components/Footer";
 function App() {
-<<<<<<< HEAD
-        return (<>
-                <Footer>
-
-                </Footer>
-        </>
-        );
-}
-
-export default App;
-=======
     return (<>
         <Footer>
 
@@ -21,4 +10,3 @@ export default App;
   }
   
   export default App;
->>>>>>> 1e5cb5d630834b01050d1a54bd35d8dfa9c732a1
