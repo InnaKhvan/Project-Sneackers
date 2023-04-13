bd76803db4b65cb2c63336520411f6f580f179af
Inna Khvan <in.khvan@bk.ru>
2023-04-14 00:20:38 +0800

    add BrandBlock

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import "./index.css";
 import Slider from "./components/Slider/Slider";
+import Brands from "./components/BrandBlock";
 function App() {
   return (
     <>
       <Header />
       <Slider />
+      <Brands />
     </>
   );
 }
