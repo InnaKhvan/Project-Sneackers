ce67f56eee4ae7efdacf526d942d439740c3201a
Inna Khvan <in.khvan@bk.ru>
2023-04-17 14:13:06 +0800

    deleted slider/dots

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Header from "./components/Header";
 import "./index.css";
-import Slider from "./components/Slider/Slider";
+import Slider from "./components/Slider";
 import Brands from "./components/BrandBlock";
 function App() {
   return (
