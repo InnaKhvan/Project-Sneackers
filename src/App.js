885c001c129ba6950dd65aa6ff68a9ff7375d4c9
Inna Khvan <in.khvan@bk.ru>
2023-04-19 17:14:19 +0800

    add Navigation

--- src/App.js
+++ src/App.js
@@ -5,10 +5,14 @@ import Brands from "./components/BrandBlock";
 import ApplicationForm from "./components/ApplicationForm";
 import NewBlock from "./components/New";
 import HotSale from "./components/HotSale";
+import { Outlet } from "react-router-dom";
+import MainMenuItem from "./components/MainMenuItem";
 function App() {
   return (
     <>
       <Header />
+      <Outlet />
+      <MainMenuItem className="hidden tablet:block" />
       <Slider />
       <Brands />
       <NewBlock />
