0cdfa283b7b88a6e83826eb799648f3abcc5cb4f
Inna Khvan <in.khvan@bk.ru>
2023-04-25 14:54:39 +0800

    change file

--- src/components/FilterForm/FilterBrand/index.js
+++ src/components/FilterForm/FilterBrand/index.js
@@ -3,13 +3,11 @@ import items from "../../../mockData/filter.json";
 const FilterBrand = () => {
   return (
     <>
-      <h2 className="text-h3 mb-2.5 desktop:mb-5 mt-7	">
-        {items[1].type}
-      </h2>
+      <h2 className="text-h3 mb-2.5 desktop:mb-5 mt-7	">{items[1].type}</h2>
       {items[1].name.map((item) => (
         <div className="text-p2 mb-2.5">
-          <input className="mr-1" type="checkbox" />
-          {item}
+          <input className="mr-1" type="checkbox" id="brand" />
+          <label for="brand">{item}</label>
         </div>
       ))}
     </>
--- src/components/HotSale/index.js
+++ src/components/HotSale/index.js
@@ -2,9 +2,12 @@ import styles from "./HotSale.module.css";
 const HotSale = () => {
   return (
     <div className={"container flex flex-col content-between " + styles.block}>
-      <div className={"flex text-sale justify-center text-h1 "}>HOT SALE</div>
+      <div className="flex text-sale justify-center text-h1">HOT SALE</div>
       <a
-        className={"flex font-semibold content-end justify-center underline " + styles.look}
+        className={
+          "flex font-semibold content-end justify-center underline " +
+          styles.look
+        }
         href=""
       >
         Смотреть еще
