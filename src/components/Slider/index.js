623f31036cf1ab16308f782275ee7760c277aad8
Inna Khvan <in.khvan@bk.ru>
2023-04-28 13:53:09 +0800

    changes Slider, add pagingDotsIndices

--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -1,23 +1,31 @@
 import img from "../../mockData/background.png";
 import { Link } from "react-router-dom";
-
+import Ellipse from '../svg/Ellipse'
 import Carousel from "nuka-carousel";
 import styles from "./slider.module.css";
 // import Ellipse from "../svg/Ellipse";
 export const Slider = () => {
   const slides = [img, img, img, img];
-  const params = {};
-  return (
-    <div className="flex justify-center relative">
-      <div className={"flex " + styles.carousel}>
-        <Carousel
-          {...params}
-          renderCenterLeftControls
-          // renderBottomCenterControls={{Paging dots}}
-          // renderCenterRightControls={({ goToSlide, slideIndex }) => (
-          //   <button onClick={goToSlide(slideIndex)}><Ellipse /></button>
-          // )}
-        >
+  const pagingDots = ({ goToSlide, currentSlide, slideIndex, pagingDotsIndices }) =>
+  pagingDotsIndices.map((index) => (
+    <button goToSlide={index}
+      style={{
+        display: "flex",
+        marginRight: 120,
+        padding: 11
+      }}
+    >
+      <Ellipse />
+    </button>
+  ));
+return (
+  <div className="flex justify-center relative">
+    <div className={"flex " + styles.carousel}>
+      <Carousel
+        renderCenterRightControls={pagingDots}
+        renderCenterLeftControls
+        renderBottomCenterControls
+      >
           {slides.map((slide) => (
             <img src={slide} key={slide} alt="" />
           ))}
