74b7664fa9514b2399e1ae413a0b584e710afd24
Arsalan Rabdanov <uzarsalan@gmail.com>
2023-04-28 23:34:53 +0300

    homepage + basename router fix

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "../App"; 
+import App from "../App";
 import Catalog from "./Catalog";
 import MainPage from "./MainPage";
 import Order from "./Order";
-const router = createBrowserRouter([
-    { 
+const router = createBrowserRouter(
+  [
+    {
       path: "/",
       element: <App />,
       children: [
@@ -15,14 +16,16 @@ const router = createBrowserRouter([
         {
           path: "Catalog",
           element: <Catalog />,
-        }
-      ]
+        },
+      ],
     },
-   
+
     {
       path: "Order",
       element: <Order />,
     },
-])
+  ],
+  { basename: "/Project-Sneackers" }
+);
 
 export default router;
