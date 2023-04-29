ebf6c907f4a4d0a4f88c177fa0646421a7a7a332
Inna Khvan <in.khvan@bk.ru>
2023-04-29 22:57:40 +0800

    change MenuItems. add width 1/3

--- src/components/MenuItems/MenuItem/index.js
+++ src/components/MenuItems/MenuItem/index.js
@@ -1,13 +1,13 @@
-import './menuitem.css'
 import { Link } from "react-router-dom";
+import styles from "./menuitem.module.css"
 import items from "../../../mockData/menuItem.json";
-const MenuItem = ({ kind}) => {
+const MenuItem = ({kind}) => {
   const className = kind === "primary" ? "BlackColor" : "RedColor";
   return (
     <>
       {items.map((item) => (
         <Link>
-          <button className={`pr-9 ${className}`}>{item.name}</button>
+          <button className={className}>{item.name}</button>
         </Link>
       ))}
     </>
