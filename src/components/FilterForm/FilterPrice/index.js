8e157a37550a99afc5e992dc849c5a35067b0b4e
Inna Khvan <in.khvan@bk.ru>
2023-04-29 22:59:47 +0800

    delete menuitem.css

--- src/components/FilterForm/FilterPrice/index.js
+++ src/components/FilterForm/FilterPrice/index.js
@@ -2,24 +2,8 @@ import RangeSlider from "react-range-slider-input";
 import "react-range-slider-input/dist/style.css";
 import "./style.css";
 import styles from './filterPrice.module.css'
-// rangeSlider = {
-//     min: 150,
-//     max: 250,
 
-// };
 const FilterPrice = () => {
-
-  const rng=document.getElementById('r1'); //rng - это ползунок
-  const i1=document.getElementById('i1'); // i1 - input
- i1=rng;
-
-  // const rangeSliderElement = {
-  //   min: 150,
-  //   max: 450,
-    
-  
-  // };
-
 
   return (
     <div className={styles.mainblock}>
@@ -30,7 +14,7 @@ const FilterPrice = () => {
         <input className={"text-p2 border pl-2.5 pr-5	bg-secondary1 border-secondary2 " + styles.priceblock} />
       </div>
 
-      <RangeSlider type="range" id="r1" oninput="FilterPrice" />
+      <RangeSlider  />
     </div>
   );
 };
--- src/components/MenuItems/index.js
+++ src/components/MenuItems/index.js
@@ -1,9 +1,13 @@
+import MenuItem from "./MenuItem";
 
 const MenuItems = () => {
   return (
     <div className="bg-secondary1 hidden tablet:flex">
       <div className="container flex justify-start items-center h-14 text-xs font-semibold">
-       
+        <div className="flex w-1/3 justify-between">
+        <MenuItem />
+        </div>
+     
       </div>
     </div>
   );
