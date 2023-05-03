58f036b4c4c5a8ce0cf9adca278e3fa0d7f91b38
Inna Khvan <in.khvan@bk.ru>
2023-05-03 17:49:49 +0800

    deleted inputElem css

--- src/components/OrderForm/ContactForm/index.js
+++ src/components/OrderForm/ContactForm/index.js
@@ -5,9 +5,9 @@ const ContactForm = () => {
   return (
     <OrderCard title="Укажите ваши контактные данные">
       <div className="flex desktop:flex-row flex-col ">
-        <InputElem className={"mb-2.5	PrimaryInput"} placeholder="Ваше имя" />
+        <InputElem className="Primary mb-2.5" placeholder="Ваше имя" />
         <div className="desktop:ml-5">
-          <InputElem className={"PrimaryInput"} placeholder="Номер телефона" />
+          <InputElem className="PrimaryInput" placeholder="Номер телефона" />
         </div>
       </div>
     </OrderCard>
