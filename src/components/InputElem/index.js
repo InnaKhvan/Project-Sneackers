60a7150fed34bd936ce2c47471a6d8208e75d335
Inna Khvan <in.khvan@bk.ru>
2023-05-03 17:48:53 +0800

    deleted inputElem.css

--- src/components/InputElem/index.js
+++ src/components/InputElem/index.js
@@ -1,8 +1,11 @@
 import "./inputElem.css";
 import React from "react";
 const InputElem = ({ kind, ...other }) => {
-  const className = kind === "primary" ? "PrimaryInput" : "SecondaryInput";
-  return <input className={className} {...other} />;
+  
+  const className = kind === "Primary" ? "PrimaryInput" : "SecondaryInput";
+  return <input className={"border-secondary2 rounded-xl bg-secondary1 h-14 w-64 border-2 text-p1 p-5 " + className} {...other}/>;
+
+  // border-secondary2 rounded-xl bg-secondary1 h-14 w-64 border-2 text-p1 p-5
 };
 
 export default InputElem;
