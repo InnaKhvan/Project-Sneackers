20543be7dcd06ed696f81e76e8f6f8d6e82b8bdb
Inna Khvan <in.khvan@bk.ru>
2023-05-03 22:03:32 +0800

    add input id

--- src/components/FilterForm/FilterBrand/index.js
+++ src/components/FilterForm/FilterBrand/index.js
@@ -6,7 +6,7 @@ const FilterBrand = () => {
       <h2 className="text-h3 mb-2.5 desktop:mb-5 mt-7	">{items[1].type}</h2>
       {items[1].name.map((item) => (
         <div className="text-p2 mb-2.5">
-          <input className="mr-1" type="checkbox" id="brand" />
+          <input className="mr-1" type="checkbox" id={`brand - ${item}`} />
           <label for="brand">{item}</label>
         </div>
       ))}
