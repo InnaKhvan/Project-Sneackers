21d05dde730f1c785710b8887201bd5466669127
Inna Khvan <in.khvan@bk.ru>
2023-05-04 17:06:24 +0800

    add dot in slider, deleted some css

--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -1,51 +1,40 @@
 import img from "../../mockData/slider.json";
 import { Link } from "react-router-dom";
-import Dots from "../svg/Dots";
 import Carousel from "nuka-carousel";
 import styles from "./slider.module.css";
-import ActiveDot from "../svg/ActiveDot";
 export const Slider = () => {
   const pagingDots = ({ goToSlide, slideIndex, pagingDotsIndices }) =>
     pagingDotsIndices.map((index) => (
       <button
         onClick={() => goToSlide(index)}
-        style={{
-          display: "flex",
-          marginRight: 120,
-          padding: 11,
-        }}
-        // className={index == slideIndex ? styles.dotActive : styles.dot}
-      >
-        <Dots />
-      </button>
+        className={index == slideIndex ? styles.dotActive : styles.dot}
+      />
     ));
   return (
     <div className="flex justify-center relative">
-      <div className={"flex " + styles.carousel}>
-        <Carousel
-          renderTopCenterControls={({ currentSlide }) => (
-            <div>Slide: {currentSlide}</div>
-          )}
-          animation="zoom"
-          // currentSlide
-          renderCenterRightControls={pagingDots}
-          renderCenterLeftControls
-          renderBottomCenterControls
-        >
-          {img.map((slide) => (
-            <img src={slide.img} key={slide} alt="" />
-          ))}
-        </Carousel>
-      </div>
-      <div className={"flex absolute " + styles.catalog}>
+      <Carousel
+        renderTopCenterControls={({ currentSlide }) => (
+          <div>Slide: {currentSlide}</div>
+        )}
+        animation="zoom"
+        // currentSlide={0}
+        renderCenterRightControls={pagingDots}
+        renderCenterLeftControls
+        renderBottomCenterControls
+      >
+        {img.map((slide) => (
+          <img
+            className="w-full h-[605px] object-cover"
+            src={slide.img}
+            key={slide}
+            alt=""
+          />
+        ))}
+      </Carousel>
+      <div className="flex absolute bottom-[50px]">
         {" "}
         <Link to="Catalog">
-          <button
-            className={
-              "flex bg-primary1 text-p1 justify-center text-secondary1 p-5 rounded-xl " +
-              styles.button
-            }
-          >
+          <button className="flex bg-primary1 text-p1 justify-center text-secondary1 p-5 rounded-xl w-[204px] h-[60px]">
             Смотреть каталог{" "}
           </button>
         </Link>
