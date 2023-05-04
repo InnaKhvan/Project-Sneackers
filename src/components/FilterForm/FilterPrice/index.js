6f14c577d03ab9b95bc430c6d565a75535976083
Inna Khvan <in.khvan@bk.ru>
2023-05-04 11:12:18 +0800

    change filter Price

--- src/components/FilterForm/FilterPrice/index.js
+++ src/components/FilterForm/FilterPrice/index.js
@@ -2,23 +2,23 @@ import RangeSlider from "react-range-slider-input";
 import "react-range-slider-input/dist/style.css";
 import "./style.css";
 import styles from './filterPrice.module.css'
-// rangeSlider = {
-//     min: 150,
-//     max: 250,
 
-// };
 const FilterPrice = () => {
+rangeSlider = {
+    min: 150,
+    max: 250,
 
+};
   return (
     <div className={styles.mainblock}>
       <h2 className="text-h3 mb-2.5 desktop:mb-5 mt-7">Цена</h2>
       <div className={"flex space-x-5 " + styles.block}>
         {" "}
-        <input type="text" id="i1" className={"text-p2 border pl-2.5 pr-5	bg-secondary1 border-secondary2 " + styles.priceblock} />
+        <input className={"text-p2 border pl-2.5 pr-5	bg-secondary1 border-secondary2 " + styles.priceblock} />
         <input className={"text-p2 border pl-2.5 pr-5	bg-secondary1 border-secondary2 " + styles.priceblock} />
       </div>
 
-      <RangeSlider type="range" id="r1" oninput="FilterPrice" />
+      <RangeSlider type="range" oninput="FilterPrice" />
     </div>
   );
 };
