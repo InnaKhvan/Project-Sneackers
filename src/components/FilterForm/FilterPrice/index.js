f46c8c8cc0dc0540e79f221823de0ab5df9b0ed5
Inna Khvan <in.khvan@bk.ru>
2023-05-08 10:39:47 +0800

    add RangeSlider

--- src/components/FilterForm/FilterPrice/index.js
+++ src/components/FilterForm/FilterPrice/index.js
@@ -1,7 +1,8 @@
-import RangeSlider from "react-range-slider-input";
-import "react-range-slider-input/dist/style.css";
-import "./style.css";
-import styles from './filterPrice.module.css'
+// import RangeSlider from "react-range-slider-input";
+// import "react-range-slider-input/dist/style.css";
+// import "./style.css";
+import RangeSliderComp from "../RangeSlider";
+import styles from "./filterPrice.module.css";
 
 const FilterPrice = () => {
   return (
@@ -9,13 +10,33 @@ const FilterPrice = () => {
       <h2 className="text-h3 mb-2.5 desktop:mb-5 mt-7">Цена</h2>
       <div className={"flex space-x-5 " + styles.block}>
         {" "}
-        <input className={"text-p2 border pl-2.5 pr-5	bg-secondary1 border-secondary2 " + styles.priceblock} />
-        <input className={"text-p2 border pl-2.5 pr-5	bg-secondary1 border-secondary2 " + styles.priceblock} />
+        <label>
+          {" "}
+          <input
+            type="number"
+            className={
+              "text-p2 border pl-2.5 pr-5	bg-secondary1 border-secondary2 " +
+              styles.priceblock
+            }
+          />
+        </label>
+        <label>
+          {" "}
+          <input
+            type="number"
+            min="100"
+            max="1000"
+            className={
+              "text-p2 border pl-2.5 pr-5	bg-secondary1 border-secondary2 " +
+              styles.priceblock
+            }
+          />
+        </label>
       </div>
 
-      <RangeSlider type="range" />
+      <RangeSliderComp />
+      {/* <RangeSlider type="range" /> */}
     </div>
   );
 };
-console.log(RangeSlider)
 export default FilterPrice;
---
+++ src/components/FilterForm/RangeSlider/index.js
@@ -0,0 +1,17 @@
+import RangeSlider from "react-range-slider-input";
+import "react-range-slider-input/dist/style.css";
+import "./style.css";
+
+const RangeSliderComp = () => {
+  
+
+
+    return (
+        <RangeSlider />
+        
+    )
+ 
+
+}
+
+export default RangeSliderComp;
