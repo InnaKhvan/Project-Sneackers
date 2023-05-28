db025960e52e5ddab954a1165a330462578d8ef2
Inna Khvan <in.khvan@bk.ru>
2023-05-28 19:48:12 +0600

    deleted filterPrice.module.css add useState in RangeSlider

--- src/components/FilterForm/FilterPrice/index.js
+++ src/components/FilterForm/FilterPrice/index.js
@@ -1,41 +1,44 @@
-// import RangeSlider from "react-range-slider-input";
-// import "react-range-slider-input/dist/style.css";
-// import "./style.css";
-import RangeSliderComp from "../RangeSlider";
-import styles from "./filterPrice.module.css";
+import RangeSlider from "react-range-slider-input";
+import "react-range-slider-input/dist/style.css";
+import "./style.css";
+import { useState } from "react";
+
+// const Min = 100;
+// const Max = 1000;
 
 const FilterPrice = () => {
+  // const [min, max] = useState([100,1000]);
+  const [values, setValues] = useState([100, 1000]);
+
+  console.log(values);
   return (
-    <div className={styles.mainblock}>
+    <div className="mb-[30px]">
       <h2 className="text-h3 mb-2.5 desktop:mb-5 mt-7">Цена</h2>
-      <div className={"flex space-x-5 " + styles.block}>
+      <div className="flex space-x-5 mb-[15px]">
         {" "}
         <label>
           {" "}
           <input
+            value={values[0]}
             type="number"
-            className={
-              "text-p2 border pl-2.5 pr-5	bg-secondary1 border-secondary2 " +
-              styles.priceblock
-            }
+            className="text-p2 border pl-2.5 w-[50px] rounded-[3px] py-[5px] bg-secondary1 border-secondary2"
+            placeholder={values[0]}
+            onChange={(event) => setValues([values[0], event.target.value])}
           />
         </label>
         <label>
           {" "}
           <input
+            value={values[1]}
             type="number"
-            min="100"
-            max="1000"
-            className={
-              "text-p2 border pl-2.5 pr-5	bg-secondary1 border-secondary2 " +
-              styles.priceblock
-            }
+            className="text-p2 border pl-2.5 w-[50px] rounded-[3px] py-[5px] bg-secondary1 border-secondary2"
+            placeholder={values[1]}
+            onChange={(event) => setValues([event.target.value, values[1]])}
           />
         </label>
       </div>
 
-      <RangeSliderComp />
-      {/* <RangeSlider type="range" /> */}
+      <RangeSlider onInput={setValues} value={values} min={100} max={1000} />
     </div>
   );
 };
