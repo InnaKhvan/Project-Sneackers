8a928e678b43a6659f7fb78e73d03f95aef45719
Inna Khvan <in.khvan@bk.ru>
2023-05-29 17:37:05 +0600

    add newProdact.json

---
+++ src/components/HeaderComponent/ModalSeach/index.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import { createPortal } from "react-dom";
+import { animated } from "@react-spring/web";
+import SearchIconTablet from "../../svg/SearchIconTablet";
+import SearchForm from "./../Search";
+
+const ModalSeach = () => {
+  const [open, set] = useState(false);
+
+  return (
+    <>
+      {" "}
+      <animated.div onClick={() => set(true)}>
+        <SearchIconTablet />
+      </animated.div>
+      {open &&
+        createPortal(
+          <animated.div
+            onClick={() => set(false)}
+            className="flex justify-center top-20 np fixed z-20 inset-0 "
+          >
+            <SearchForm />
+          </animated.div>,
+          document.body
+        )}
+    </>
+  );
+};
+export default ModalSeach;
