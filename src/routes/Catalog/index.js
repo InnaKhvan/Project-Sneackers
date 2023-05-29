1fec38675b984bb1d7c87146a6a9131ef1430592
Inna Khvan <in.khvan@bk.ru>
2023-05-29 12:33:15 +0600

    change Catalog delete module.css

--- src/routes/Catalog/index.js
+++ src/routes/Catalog/index.js
@@ -1,17 +1,19 @@
 import Card from "../../components/CardProduct";
 import Filter from "../../components/FilterForm";
+import FilterSetting from "../../components/FilterSetting";
 import Navigation from "../../components/Navigation";
 
 const Catalog = () => {
   return (
     <>
-      <Navigation />
+      <Navigation name="Каталог"/>
+      <FilterSetting />
       <div className="container flex space-x-6 ">
         <div className="flex w-1/4 hidden tablet:flex ">
           {" "}
           <Filter />
         </div>
-        <div className="w-3/4 grid grid-cols-3 gap-x-6 gap-y-8">
+        <div className="tablet:w-3/4 grid grid-cols-2 desktop:grid-cols-3 gap-x-6 gap-y-8">
           <Card />
         </div>
       </div>
