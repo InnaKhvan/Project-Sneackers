38b0f39fe838aed112bb029dfb6bb81d449023ca
Inna Khvan <in.khvan@bk.ru>
2023-06-11 20:26:09 +0600

    add style in CardComponent

--- src/components/CardComponent/index.js
+++ src/components/CardComponent/index.js
@@ -5,27 +5,40 @@ import HeardVector from "../svg/HeardVector";
 
 const CardComponent = ({ item }) => {
   return (
-    <div className="flex flex-col space-x-5 bg-secondary1">
-      <div className="flex w-1/2"><img src="/Project-Sneackers/adidas1.png" /></div>
-      <div className="flex w-1/2 flex-col">
-        <div className="flex text-h2t font-bold	">{item.name}</div>
-        <div className="text-p1">
-          Артикул: <div className="text-p1 font-semibold">{item.articul}</div>
-        </div>
-        <div className="text-p1">
-          Цвет: <div className="text-p1 font-semibold">{item.color}</div>
-        </div>
-        <div className="text-p1">
-          Пол: <div className="text-p1 font-semibold">{item.sex}</div>
-        </div>
-        <FilterSize /> <div className="text-xl font-bold">{item.price} BYN</div>
-        <div className="flex flex-row space-x-3.5">
-          {" "}
-          <button className="bg-primary2 text-secondary1 items-center	justify-center text-p2 font-bold rounded-xl h-[43px] w-[143px]">В корзину
-          </button><a className="flex w-[43px] h-[43px] border-2	items-center	justify-center	 rounded-lg border-primary2">
+    <div className="container grid grid-rows-2 gap-y-5 tablet:grid-cols-2 gap-x-4	desktop:gap-x-7	">
+      <div className="grid">
+        <img src="/Project-Sneackers/adidas1.png" />
+      </div>
+      <div className="grid">
+        <div className="flex text-h2t font-bold	mb-2.5 tablet:mb-8 text-3xl	desktop:text-h1 mb-[60px]">{item.name}</div>
+        <div className="flex flex-col space-y-5 desktop:space-y-7">
+          <div className="flex flex-col text-p1 space-y-2 desktop:text-h2 space-y-3">
+            <div className=" flex flex-row">
+              Артикул:{" "}
+              <div className="font-semibold">{item.articul}</div>
+            </div>
+            <div className=" flex flex-row">
+              Цвет: <div className="font-semibold">{item.color}</div>
+            </div>
+            <div className=" flex flex-row">
+              Пол: <div className="font-semibold">{item.sex}</div>
+            </div>
+          </div>
+          <FilterSize />{" "}
+          <div className="flex flex-col space-y-2.5 desktop:space-y-6">
             {" "}
-            <HeardVector />
-          </a>
+            <div className="text-xl font-bold desktop:text-h4">{item.price} BYN</div>
+            <div className="flex flex-row space-x-3.5 desktop:space-x-6">
+              {" "}
+              <button className="bg-primary2 text-secondary1 items-center	justify-center text-p2 font-bold rounded-xl h-[43px] w-[143px] desktop:w-60 desktop:h-[72px] desktop:text-h2m">
+                В корзину
+              </button>
+              <a className="flex w-[43px] h-[43px] border-2	items-center justify-center rounded-lg border-primary2 desktop:w-[72px] desktop:h-[72px]">
+                {" "}
+                <HeardVector />
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/routes/CardItem/index.js
+++ src/routes/CardItem/index.js
@@ -1,10 +1,12 @@
 import CardComponent from "../../components/CardComponent";
+import MenuItems from "../../components/MenuItems";
 import Navigation from "../../components/Navigation";
 import items from "../../mockData/products.json";
 
 const CardItem = () => {
   return (
     <>
+    <MenuItems className="hidden tablet:block"/>
       <Navigation name="Товар" />
       {items.map((item) => (
         <CardComponent item={item} />
