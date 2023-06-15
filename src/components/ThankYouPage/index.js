0594b6e1bf774dee1492b442cd42fedd78836e6c
Dean1501 <damdus@bk.ru>
2023-06-15 13:39:29 +0300

    add text

--- src/components/ThankYouPage/index.js
+++ src/components/ThankYouPage/index.js
@@ -1,12 +1,17 @@
-import BreadCrumbs from "../svg/BreadCrumbs";
+import Navigation from "../Navigation";
+import ThankYouUnderText from "../svg/ThankYouUnderText";
+import ThankYouText from "../svg/ThannkYouText";
 import TickSymbolThanksPage from "../svg/TickSymbolThanksPage";
+import React from "react";
 
 function ThankYouPage() {
     return (
-        <div className="flex">
+        <div className="container md:mx-auto">
             <div className="md:container md:mx-auto">
-                <BreadCrumbs className="left-0 top-0 h-16 w-16" />
-                <TickSymbolThanksPage className="place-content-center inset-0" />
+                <Navigation name="Оформление заказа"></Navigation>
+                <TickSymbolThanksPage className="place-items-center" />
+                <ThankYouText></ThankYouText>
+                <ThankYouUnderText></ThankYouUnderText>
             </div>
         </div>
     )
