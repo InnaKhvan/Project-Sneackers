8e75108fe0f3eb10735853b9db6e54b887a9fb0d
Dean1501 <damdus@bk.ru>
2023-06-28 15:51:13 +0300

    fix margins

--- src/components/ThankYouPage/index.js
+++ src/components/ThankYouPage/index.js
@@ -8,7 +8,7 @@ function ThankYouPage() {
     return (
         <div className="container md:mx-auto">
             <Navigation name="Оформление заказа"></Navigation>
-            <div className="flex flex-col items-center gap-[2.75rem] m-20">
+            <div className="flex flex-col mt-[154px] mb-[146px] ml-[382px] mr-[389px] items-center gap-[2.75rem]">
                 <div><TickSymbolThanksPage /></div>
                 <div><ThankYouText></ThankYouText></div>
                 <div><ThankYouUnderText></ThankYouUnderText></div>
